4bcd8be9e6f1b3370f157e7d26b4f6549b2bbd56
Arkaiz <jorgearkay@gmail.com>
2025-01-21 10:59:30 +0100

    Update rutas_juegos.py

--- apiProyecto2/web/rutas_juegos*.py
+++ apiProyecto2/web/rutas_juegos*.py
@@ -8,38 +8,38 @@ class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal): return float(obj)
 
-@app.route("/juegos",methods=["GET"])
+@app.route("/muebles",methods=["GET"])
 def juegos():
-    juegos,code= controlador_juegos.obtener_juegos()
-    return json.dumps(juegos, cls = Encoder),code
+    muebles,code= controlador_muebles.obtener_muebles()
+    return json.dumps(muebles, cls = Encoder),code
 
-@app.route("/juego/<id>",methods=["GET"])
-def juego_por_id(id):
-    juego,code = controlador_juegos.obtener_juego_por_id(id)
-    return json.dumps(juego, cls = Encoder),code
+@app.route("/muebles/<id>",methods=["GET"])
+def muebles_por_id(id):
+    muebles,code = controlador_muebles.obtener_muebles_por_id(id)
+    return json.dumps(mueble, cls = Encoder),code
 
-@app.route("/juegos",methods=["POST"])
-def guardar_juego():
+@app.route("/muebles",methods=["POST"])
+def guardar_mueble():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        juego_json = request.json
-        ret,code=controlador_juegos.insertar_juego(juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]), juego_json["foto"])
+        mueble_json = request.json
+        ret,code=controlador_muebles.insertar_mueble(mueble_json["nombre"], mueble_json["descripcion"], float(mueble_json["precio"]), mueble_json["foto"])
     else:
         ret={"status":"Bad request"}
         code=401
     return json.dumps(ret), code
 
 @app.route("/juegos/<id>", methods=["DELETE"])
-def eliminar_juego(id):
-    ret,code=controlador_juegos.eliminar_juego(id)
+def eliminar_mueble(id):
+    ret,code=controlador_muebles.eliminar_mueble(id)
     return json.dumps(ret), code
 
-@app.route("/juegos", methods=["PUT"])
-def actualizar_juego():
+@app.route("/muebles", methods=["PUT"])
+def actualizar_mueble():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        juego_json = request.json
-        ret,code=controlador_juegos.actualizar_juego(juego_json["id"],juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]),juego_json["foto"])
+        mueble_json = request.json
+        ret,code=controlador_muebles.actualizar_mueble(mueble_json["id"],mueble_json["nombre"], mueble_json["descripcion"], float(mueble_json["precio"]),mueble_json["foto"])
     else:
         ret={"status":"Bad request"}
         code=401
