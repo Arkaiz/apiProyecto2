827df9fead56ab95734969cf962945bf031b74cf
arkaiz <jorgearkay@gmail.com>
2025-01-24 11:09:30 +0100

    version 1

---
+++ Muebles.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS PEPS;
+USE PEPS;
+CREATE TABLE muebles(
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    alto DECIMAL(6,2) NOT NULL,       
+    ancho DECIMAL(6,2) NOT NULL,       
+    largo DECIMAL(6,2) NOT NULL,
+    precio DECIMAL(9,2) NOT NULL,
+	foto VARCHAR(255)
+);
+CREATE TABLE usuarios(
+	usuario VARCHAR(100) NOT NULL PRIMARY KEY,
+    clave VARCHAR(255) NOT NULL,
+    perfil VARCHAR(100) NOT NULL,
+    fechaUltimoAcceso DATE
+);
+INSERT INTO `usuarios` (`usuario`, `clave`, `perfil`, `fechaUltimoAcceso`) VALUES ('root', '1234', 'admin', NOW());
