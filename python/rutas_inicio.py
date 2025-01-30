e76bfe0e65cf14d8c7c3aad80cf68664a39006d4
arkaiz <jorgearkay@gmail.com>
2025-01-30 12:31:35 +0100

    Apache arreglado

--- python/rutas_inicio.py
+++ python/rutas_inicio.py
@@ -1,79 +1,42 @@
 from __future__ import print_function
 from __main__ import app
-from flask import request,session
+from flask import request, session
 from bd import obtener_conexion
 import json
 import sys
+import funciones_auxiliares
+import controlador_usuarios
 
-@app.route("/login",methods=['POST'])
+@app.route("/login", methods=['POST'])
 def login():
     content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        muebles_json = request.json
-        username = muebles_json['username']
-        password = muebles_json['password']
-        try:
-            conexion = obtener_conexion()
-            with conexion.cursor() as cursor:
-                 #cursor.execute("SELECT perfil FROM usuarios WHERE usuario = %s and clave= %s",(username,password))
-                 cursor.execute("SELECT perfil FROM usuarios WHERE usuario = '" + username +"' and clave= '" + password + "'")
-                 usuario = cursor.fetchone()
-            conexion.close()
-            if usuario is None:
-                ret = {"status": "ERROR","mensaje":"Usuario/clave erroneo" }
-            else:
-                ret = {"status": "OK" }
-                session["usuario"]=username
-                session["perfil"]=usuario[0]
-            code=200
-        except:
-            print("Excepcion al validar al usuario")   
-            ret={"status":"ERROR"}
-            code=500
+    if content_type == 'application/json':
+        mueble_json = request.json
+        username = mueble_json['username']
+        password = mueble_json['password']
+        respuesta, code = controlador_usuarios.login_usuario(username, password)
+        return json.dumps(respuesta, cls=funciones_auxiliares.Encoder), code
     else:
-        ret={"status":"Bad request"}
-        code=401
+        ret = {"status": "Bad request"}
+        code = 401
     return json.dumps(ret), code
 
-@app.route("/registro",methods=['POST'])
+@app.route("/registro", methods=['POST'])
 def registro():
     content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        muebles_json = request.json
-        username = muebles_json['username']
-        password = muebles_json['password']
-        perfil = muebles_json['profile']
-        try:
-            conexion = obtener_conexion()
-            with conexion.cursor() as cursor:
-                 #cursor.execute("SELECT perfil FROM usuarios WHERE usuario = %s and clave= %s",(username,password))
-                 cursor.execute("SELECT perfil FROM usuarios WHERE usuario = '" + username +"' and clave= '" + password + "'")
-                 usuario = cursor.fetchone()
-                 if usuario is None:
-                     print("INSERT INTO usuarios(usuario,clave,perfil) VALUES('"+ username +"','"+  password+"','"+ perfil+"')") 
-                     cursor.execute("INSERT INTO usuarios(usuario,clave,perfil) VALUES('"+ username +"','"+  password+"','"+ perfil+"')")
-                     if cursor.rowcount == 1:
-                         conexion.commit()
-                         ret={"status": "OK" }
-                         code=200
-                     else:
-                         ret={"status": "ERROR" }
-                         code=500
-                 else:
-                   ret = {"status": "ERROR","mensaje":"Usuario/clave erroneo" }
-                   code=200
-            conexion.close()
-        except:
-            print("Excepcion al registrar al usuario")   
-            ret={"status":"ERROR"}
-            code=500
+    if content_type == 'application/json':
+        mueble_json = request.json
+        username = mueble_json['username']
+        password = mueble_json['password']
+        perfil = mueble_json['profile']
+        respuesta, code = controlador_usuarios.alta_usuario(username, password, perfil)
+        return json.dumps(respuesta, cls=funciones_auxiliares.Encoder), code
     else:
-        ret={"status":"Bad request"}
-        code=401
+        ret = {"status": "Bad request"}
+        code = 401
     return json.dumps(ret), code
 
-
-@app.route("/logout",methods=['GET'])
+@app.route("/logout", methods=['GET'])
 def logout():
     session.clear()
-    return json.dumps({"status":"OK"}),200
+    return json.dumps({"status": "OK"}), 200
